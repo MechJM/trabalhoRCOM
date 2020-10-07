f9d666cd988ebafdb555f785070d04fef6d64336
MechJM <matosjoaops@protonmail.com>
2020-10-07 19:45:10 +0100

    Implemented functions to generate trams

--- src/tram.c
+++ src/tram.c
@@ -1,3 +1,66 @@
 #include "tram.h"
 
-void func(){return;}
+void setup_rs()
+{
+    r = 1;
+    s = 0;
+}
+
+char * generate_info_tram(char * data,unsigned char address,int array_size)
+{
+    char * tram = malloc((6 + array_size) * sizeof(unsigned char));
+    
+    tram[0] = FLAG;
+    tram[1] = address;
+
+    unsigned char actual_control = INFO_CTRL;
+
+    if (s > 0)
+    {
+        actual_control |= S_MASK;
+        s--;
+    }
+    else s++;
+
+    tram[2] = actual_control;
+    tram[3] = (FLAG ^ address) ^ actual_control;
+
+    unsigned char bcc2 = 0x00;
+
+    for (int i = 4; i < (array_size + 4); i++)
+    {
+        tram[i] = data[i - 4];
+        bcc2 ^= tram[i];
+    }
+
+    tram[4 + array_size] = bcc2;
+    tram[5 + array_size] = FLAG;
+
+    return tram;
+}
+
+char * generate_su_tram(unsigned char address, unsigned char control)
+{
+    unsigned char * tram = malloc(5 * sizeof(unsigned char));
+    tram[0] = FLAG;
+    tram[1] = address;
+
+    unsigned char actual_control = control;
+
+    if (control == RR || control == REJ)
+    {
+        if (r > 0)
+        {
+            actual_control |= R_MASK;
+            r--;
+        }
+        else r++;
+    }
+    
+
+    tram[2] = actual_control;
+    tram[3] = (FLAG ^ address) ^ actual_control;
+    tram[4] = FLAG;
+
+    return tram;
+}
--- src/tram.h
+++ src/tram.h
@@ -1,3 +1,22 @@
 #pragma once
 
-void func();
+#define INFO_CTRL 0x00
+#define FLAG      0x7e
+#define SET       0x03
+#define DISC      0x0b
+#define UA        0x07
+#define RR        0x05
+#define REJ       0x01
+#define R_MASK    0x80
+#define S_MASK    0x40
+
+#define COMM_SEND_REP_REC 0x03
+#define COMM_REC_REP_SEND 0x01
+
+int r,s;
+
+void setup_rs();
+
+char * generate_info_tram(char * data,unsigned char address,int array_size);
+
+char * generate_su_tram(unsigned char address, unsigned char control);
