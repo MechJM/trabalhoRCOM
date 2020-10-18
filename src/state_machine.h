d7850349a8e32ad9b2c4daadba4270d2b199e243
MechJM <matosjoaops@protonmail.com>
2020-10-18 16:55:45 +0100

    Added forgotten files related to previous commit

---
+++ src/state_machine.h
@@ -0,0 +1,17 @@
+#include "tram.h"
+
+#pragma once
+
+int timeout;
+
+enum set_states
+{
+    start,
+    flag_rcv,
+    a_rcv,
+    c_rcv,
+    bcc_ok,
+    stop
+};
+
+unsigned char * receive_tram(int control, int fd);
