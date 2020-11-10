0f2415fc7545cab253890af9f22a4da37a4bd7fd
MechJM <matosjoaops@protonmail.com>
2020-11-10 23:04:33 +0000

    Finished scripts to automate efficiency testing

--- scripts/test_reader.sh
+++ scripts/test_reader.sh
@@ -1,50 +1,97 @@
 #!/bin/bash
 touch results_reader.csv
 # Parameters: port baudrate packet_size FER T_prop
+# Changing FER
+./read 0 B38400 1000 10 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 10 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 10 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 20 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 20 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 20 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 30 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 30 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 30 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 40 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 40 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 40 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 50 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 50 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 50 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 60 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 60 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 60 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 70 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 70 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 70 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 80 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 80 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 80 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 90 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 90 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 90 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 100 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 100 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 100 0 && rm pinguim_clone.gif
+# Changing T_prop
+./read 0 B38400 1000 0 1 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 1 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 1 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 2 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 2 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 2 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 3 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 3 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 3 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 4 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 4 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 4 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 5 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 5 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 5 && rm pinguim_clone.gif
 # Changing baudrate
-./read 0 B300 200 0 0 && rm pinguim_clone.gif
-./read 0 B300 200 0 0 && rm pinguim_clone.gif
-./read 0 B300 200 0 0 && rm pinguim_clone.gif
-./read 0 B900 200 0 0 && rm pinguim_clone.gif
-./read 0 B900 200 0 0 && rm pinguim_clone.gif
-./read 0 B900 200 0 0 && rm pinguim_clone.gif
-./read 0 B2700 200 0 0 && rm pinguim_clone.gif
-./read 0 B2700 200 0 0 && rm pinguim_clone.gif
-./read 0 B2700 200 0 0 && rm pinguim_clone.gif
-./read 0 B8100 200 0 0 && rm pinguim_clone.gif
-./read 0 B8100 200 0 0 && rm pinguim_clone.gif
-./read 0 B8100 200 0 0 && rm pinguim_clone.gif
-./read 0 B19200 200 0 0 && rm pinguim_clone.gif
-./read 0 B19200 200 0 0 && rm pinguim_clone.gif
-./read 0 B19200 200 0 0 && rm pinguim_clone.gif
-./read 0 B38400 200 0 0 && rm pinguim_clone.gif
-./read 0 B38400 200 0 0 && rm pinguim_clone.gif
-./read 0 B38400 200 0 0 && rm pinguim_clone.gif
-./read 0 B76800 200 0 0 && rm pinguim_clone.gif
-./read 0 B76800 200 0 0 && rm pinguim_clone.gif
-./read 0 B76800 200 0 0 && rm pinguim_clone.gif
-./read 0 B115200 200 0 0 && rm pinguim_clone.gif
-./read 0 B115200 200 0 0 && rm pinguim_clone.gif
-./read 0 B115200 200 0 0 && rm pinguim_clone.gif
+./read 0 B300 1000 0 0 && rm pinguim_clone.gif
+./read 0 B300 1000 0 0 && rm pinguim_clone.gif
+./read 0 B300 1000 0 0 && rm pinguim_clone.gif
+./read 0 B900 1000 0 0 && rm pinguim_clone.gif
+./read 0 B900 1000 0 0 && rm pinguim_clone.gif
+./read 0 B900 1000 0 0 && rm pinguim_clone.gif
+./read 0 B2700 1000 0 0 && rm pinguim_clone.gif
+./read 0 B2700 1000 0 0 && rm pinguim_clone.gif
+./read 0 B2700 1000 0 0 && rm pinguim_clone.gif
+./read 0 B8100 1000 0 0 && rm pinguim_clone.gif
+./read 0 B8100 1000 0 0 && rm pinguim_clone.gif
+./read 0 B8100 1000 0 0 && rm pinguim_clone.gif
+./read 0 B19200 1000 0 0 && rm pinguim_clone.gif
+./read 0 B19200 1000 0 0 && rm pinguim_clone.gif
+./read 0 B19200 1000 0 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 0 && rm pinguim_clone.gif
+./read 0 B38400 1000 0 0 && rm pinguim_clone.gif
+./read 0 B76800 1000 0 0 && rm pinguim_clone.gif
+./read 0 B76800 1000 0 0 && rm pinguim_clone.gif
+./read 0 B76800 1000 0 0 && rm pinguim_clone.gif
+./read 0 B115200 1000 0 0 && rm pinguim_clone.gif
+./read 0 B115200 1000 0 0 && rm pinguim_clone.gif
+./read 0 B115200 1000 0 0 && rm pinguim_clone.gif
 # Changing packet size
-./read 0 B38400 512 0 0 && rm pinguim_clone.gif
-./read 0 B38400 512 0 0 && rm pinguim_clone.gif
-./read 0 B38400 512 0 0 && rm pinguim_clone.gif
-./read 0 B38400 1024 0 0 && rm pinguim_clone.gif
-./read 0 B38400 1024 0 0 && rm pinguim_clone.gif
-./read 0 B38400 1024 0 0 && rm pinguim_clone.gif
-./read 0 B38400 2048 0 0 && rm pinguim_clone.gif
-./read 0 B38400 2048 0 0 && rm pinguim_clone.gif
-./read 0 B38400 2048 0 0 && rm pinguim_clone.gif
-./read 0 B38400 4096 0 0 && rm pinguim_clone.gif
-./read 0 B38400 4096 0 0 && rm pinguim_clone.gif
-./read 0 B38400 4096 0 0 && rm pinguim_clone.gif
-./read 0 B38400 8192 0 0 && rm pinguim_clone.gif
-./read 0 B38400 8192 0 0 && rm pinguim_clone.gif
-./read 0 B38400 8192 0 0 && rm pinguim_clone.gif
-./read 0 B38400 16384 0 0 && rm pinguim_clone.gif
-./read 0 B38400 16384 0 0 && rm pinguim_clone.gif
-./read 0 B38400 16384 0 0 && rm pinguim_clone.gif
-./read 0 B38400 32768 0 0 && rm pinguim_clone.gif
-./read 0 B38400 32768 0 0 && rm pinguim_clone.gif
-./read 0 B38400 32768 0 0 && rm pinguim_clone.gif
+./read 0 B38400 512 && rm pinguim_clone.gif
+./read 0 B38400 512 && rm pinguim_clone.gif
+./read 0 B38400 512 && rm pinguim_clone.gif
+./read 0 B38400 1024 && rm pinguim_clone.gif
+./read 0 B38400 1024 && rm pinguim_clone.gif
+./read 0 B38400 1024 && rm pinguim_clone.gif
+./read 0 B38400 2048 && rm pinguim_clone.gif
+./read 0 B38400 2048 && rm pinguim_clone.gif
+./read 0 B38400 2048 && rm pinguim_clone.gif
+./read 0 B38400 4096 && rm pinguim_clone.gif
+./read 0 B38400 4096 && rm pinguim_clone.gif
+./read 0 B38400 4096 && rm pinguim_clone.gif
+./read 0 B38400 8192 && rm pinguim_clone.gif
+./read 0 B38400 8192 && rm pinguim_clone.gif
+./read 0 B38400 8192 && rm pinguim_clone.gif
+./read 0 B38400 16384 && rm pinguim_clone.gif
+./read 0 B38400 16384 && rm pinguim_clone.gif
+./read 0 B38400 16384 && rm pinguim_clone.gif
+./read 0 B38400 32768 && rm pinguim_clone.gif
+./read 0 B38400 32768 && rm pinguim_clone.gif
+./read 0 B38400 32768 && rm pinguim_clone.gif
--- scripts/test_writer.sh
+++ scripts/test_writer.sh
@@ -1,31 +1,78 @@
 #!/bin/bash
 touch results_writer.csv
 # Parameters: port filename baudrate packet_size FER T_prop
+# Changing FER
+./write 0 pinguim.gif B38400 1000 10 0
+./write 0 pinguim.gif B38400 1000 10 0
+./write 0 pinguim.gif B38400 1000 10 0
+./write 0 pinguim.gif B38400 1000 20 0
+./write 0 pinguim.gif B38400 1000 20 0
+./write 0 pinguim.gif B38400 1000 20 0
+./write 0 pinguim.gif B38400 1000 30 0
+./write 0 pinguim.gif B38400 1000 30 0
+./write 0 pinguim.gif B38400 1000 30 0
+./write 0 pinguim.gif B38400 1000 40 0
+./write 0 pinguim.gif B38400 1000 40 0
+./write 0 pinguim.gif B38400 1000 40 0
+./write 0 pinguim.gif B38400 1000 50 0
+./write 0 pinguim.gif B38400 1000 50 0
+./write 0 pinguim.gif B38400 1000 50 0
+./write 0 pinguim.gif B38400 1000 60 0
+./write 0 pinguim.gif B38400 1000 60 0
+./write 0 pinguim.gif B38400 1000 60 0
+./write 0 pinguim.gif B38400 1000 70 0
+./write 0 pinguim.gif B38400 1000 70 0
+./write 0 pinguim.gif B38400 1000 70 0
+./write 0 pinguim.gif B38400 1000 80 0
+./write 0 pinguim.gif B38400 1000 80 0
+./write 0 pinguim.gif B38400 1000 80 0
+./write 0 pinguim.gif B38400 1000 90 0
+./write 0 pinguim.gif B38400 1000 90 0
+./write 0 pinguim.gif B38400 1000 90 0
+./write 0 pinguim.gif B38400 1000 100 0
+./write 0 pinguim.gif B38400 1000 100 0
+./write 0 pinguim.gif B38400 1000 100 0
+# Changing T_prop
+./write 0 pinguim.gif B38400 1000 0 1
+./write 0 pinguim.gif B38400 1000 0 1
+./write 0 pinguim.gif B38400 1000 0 1
+./write 0 pinguim.gif B38400 1000 0 2
+./write 0 pinguim.gif B38400 1000 0 2
+./write 0 pinguim.gif B38400 1000 0 2
+./write 0 pinguim.gif B38400 1000 0 3
+./write 0 pinguim.gif B38400 1000 0 3
+./write 0 pinguim.gif B38400 1000 0 3
+./write 0 pinguim.gif B38400 1000 0 4
+./write 0 pinguim.gif B38400 1000 0 4
+./write 0 pinguim.gif B38400 1000 0 4
+./write 0 pinguim.gif B38400 1000 0 5
+./write 0 pinguim.gif B38400 1000 0 5
+./write 0 pinguim.gif B38400 1000 0 5
 # Changing baudrate
-./write 0 pinguim.gif B300 200 0 0
-./write 0 pinguim.gif B300 200 0 0
-./write 0 pinguim.gif B300 200 0 0
-./write 0 pinguim.gif B900 200 0 0
-./write 0 pinguim.gif B900 200 0 0
-./write 0 pinguim.gif B900 200 0 0
-./write 0 pinguim.gif B2700 200 0 0
-./write 0 pinguim.gif B2700 200 0 0
-./write 0 pinguim.gif B2700 200 0 0
-./write 0 pinguim.gif B8100 200 0 0
-./write 0 pinguim.gif B8100 200 0 0
-./write 0 pinguim.gif B8100 200 0 0
-./write 0 pinguim.gif B19200 200 0 0
-./write 0 pinguim.gif B19200 200 0 0
-./write 0 pinguim.gif B19200 200 0 0
-./write 0 pinguim.gif B38400 200 0 0
-./write 0 pinguim.gif B38400 200 0 0
-./write 0 pinguim.gif B38400 200 0 0
-./write 0 pinguim.gif B76800 200 0 0
-./write 0 pinguim.gif B76800 200 0 0
-./write 0 pinguim.gif B76800 200 0 0
-./write 0 pinguim.gif B115200 200 0 0
-./write 0 pinguim.gif B115200 200 0 0
-./write 0 pinguim.gif B115200 200 0 0
+./write 0 pinguim.gif B300 1000 0 0
+./write 0 pinguim.gif B300 1000 0 0
+./write 0 pinguim.gif B300 1000 0 0
+./write 0 pinguim.gif B900 1000 0 0
+./write 0 pinguim.gif B900 1000 0 0
+./write 0 pinguim.gif B900 1000 0 0
+./write 0 pinguim.gif B2700 1000 0 0
+./write 0 pinguim.gif B2700 1000 0 0
+./write 0 pinguim.gif B2700 1000 0 0
+./write 0 pinguim.gif B8100 1000 0 0
+./write 0 pinguim.gif B8100 1000 0 0
+./write 0 pinguim.gif B8100 1000 0 0
+./write 0 pinguim.gif B19200 1000 0 0
+./write 0 pinguim.gif B19200 1000 0 0
+./write 0 pinguim.gif B19200 1000 0 0
+./write 0 pinguim.gif B38400 1000 0 0
+./write 0 pinguim.gif B38400 1000 0 0
+./write 0 pinguim.gif B38400 1000 0 0
+./write 0 pinguim.gif B76800 1000 0 0
+./write 0 pinguim.gif B76800 1000 0 0
+./write 0 pinguim.gif B76800 1000 0 0
+./write 0 pinguim.gif B115200 1000 0 0
+./write 0 pinguim.gif B115200 1000 0 0
+./write 0 pinguim.gif B115200 1000 0 0
 # Changing packet size
 ./write 0 pinguim.gif B38400 512
 ./write 0 pinguim.gif B38400 512
