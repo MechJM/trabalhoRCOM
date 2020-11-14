8593a3bfea132cc6c520d71f00eca185127f9cc6
MechJM <matosjoaops@protonmail.com>
2020-11-14 16:36:08 +0000

    Fixed bug in avg script and updated averaged numbers

--- scripts/average_results.py
+++ scripts/average_results.py
@@ -1,5 +1,13 @@
 import sys
 
+
+def remove_newline(str):
+    result = ""
+    for i in str:
+        if i != "\n":
+            result += i
+    return result
+
 src_file_name = sys.argv[1]
 src_file = open(src_file_name,"r")
 dest_file_name = "avg_" + src_file_name.split('.')[0] + '.' + src_file_name.split('.')[1]
@@ -19,14 +27,19 @@ for i in src_lines:
         line_values = current_line.split(',')
         for j in range(num_val_keep):
             values_to_keep.append(line_values[j])
+        elems_to_count = 0
         for j in range(avg_num):
-            elem_to_append = src_lines[index + j].split(',')[-1]
+            elem_to_append = remove_newline(src_lines[index + j].split(',')[-1])
             if elem_to_append != "-1":
                 values_to_avg.append(float(elem_to_append))
-        total = 0
-        for j in values_to_avg:
-            total += j
-        avg = total / avg_num
+                elems_to_count += 1
+        if len(values_to_avg) != 0:
+            total = 0
+            for j in values_to_avg:
+                total += j
+            avg = total / elems_to_count
+        else:
+            avg = -1
         result_line = ""
         for j in values_to_keep:
             result_line += j + ','
